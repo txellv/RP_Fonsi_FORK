e2fdbf0fd4e4892ae589d160c2a3393e015d0ea0
RogerPont <roger.pont.2173@lacetania.cat>
2024-11-20 19:38:52 +0100

    Afegir nou fitxer readme i fitxers de python

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # RP_Fonsi
 
  * Contingut afegit des de la web [Github Roger Pont_Fonsi](https://github.com/RogerPont/RP_Fonsi/tree/main)
-
+ * Més contingut al fitxer a del README 2
---
+++ ex1.py
@@ -0,0 +1,26 @@
+
+número = int(input("introdueix el número: "))
+
+# nooooopoooooo
+
+while True:
+    if número % 1 == 0:
+        if número % 2 == 0:
+            print(f"{número, número2} es parell.")
+        else:
+            print(f"{número, número1} es senar.")
+
+
+
+while True:
+    
+
+
+
+
+
+
+#print(f"has introduït {número} nombres: {número} parells i {número} senar.")
+    
+#print(f"El total de tots els nombres introduits és: {número} parells i {número}, el total dels parells és: {número} parells i el total senar {número}.")
+    
